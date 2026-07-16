3eccde190237b2bae58b977fffdaef15c185c7c4
Amr <amr@programmer.net>
2026-07-16 07:40:00 +0300

    create main menu

---
+++ menus/main.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+func start() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func quit() -> void:
+	get_tree().quit()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		get_viewport().set_input_as_handled()
+		quit()
