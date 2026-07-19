6f84d55f8df9eedd9c8a45eee63d43efd210c110
Amr <amr@programmer.net>
2026-07-19 21:46:14 +0300

    reduce max enemy attack speed

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -4,7 +4,7 @@ class_name Enemy
 
 var health : float = randf_range(0.5, 10.0)
 var damage_mult : float = randf_range(0.5, 3)
-var attack_speed : int = randi_range(1, 30)
+var attack_speed : int = randi_range(1, 10)
 var accuracy : float = randf_range(0.1, 1)
 
 
