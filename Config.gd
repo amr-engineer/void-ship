5b9b5c48c14f0f8b4c8af9194a09845b61e83cc4
Amr <amr@programmer.net>
2026-07-19 10:46:17 +0300

    utilize set_value and get_value in Config

--- Config.gd
+++ Config.gd
@@ -17,13 +17,13 @@ func _ready() -> void:
 
 
 func on_config_updated() -> void:
-	DisplayServer.window_set_vsync_mode(clamp(options.get_value("graphics", "vsync", 2), 0, 3))
+	DisplayServer.window_set_vsync_mode(clamp(get_value("graphics", "vsync", 2), 0, 3))
 
-	AudioServer.set_bus_volume_db(0, options.get_value("audio", "master", 0.0))
-	AudioServer.set_bus_volume_db(1, options.get_value("audio", "sfx", 0.0))
-	AudioServer.set_bus_volume_db(2, options.get_value("audio", "background", 0.0))
-	AudioServer.set_bus_volume_db(3, options.get_value("audio", "ambient", 0.0))
-	AudioServer.set_bus_volume_db(4, options.get_value("audio", "music", 0.0))
+	AudioServer.set_bus_volume_db(0, get_value("audio", "master", 0.0))
+	AudioServer.set_bus_volume_db(1, get_value("audio", "sfx", 0.0))
+	AudioServer.set_bus_volume_db(2, get_value("audio", "background", 0.0))
+	AudioServer.set_bus_volume_db(3, get_value("audio", "ambient", 0.0))
+	AudioServer.set_bus_volume_db(4, get_value("audio", "music", 0.0))
 
 
 func save() -> Error:
