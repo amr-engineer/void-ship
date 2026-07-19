11483fb9f6554c991685f60bf077f7922e14d9bc
Amr <amr@programmer.net>
2026-07-19 10:41:30 +0300

    create config_updated signal

--- Config.gd
+++ Config.gd
@@ -1,16 +1,24 @@
 extends Node
 
 
+signal config_updated()
+
+
 const SAVE_PATH := "user://config.ini"
 
 var options := ConfigFile.new()
 
 
 func _ready() -> void:
+	config_updated.connect(on_config_updated)
+
 	options.load(SAVE_PATH)
-	
+	config_updated.emit()
+
+
+func on_config_updated() -> void:
 	DisplayServer.window_set_vsync_mode(clamp(options.get_value("graphics", "vsync", 2), 0, 3))
-	
+
 	AudioServer.set_bus_volume_db(0, options.get_value("audio", "master", 0.0))
 	AudioServer.set_bus_volume_db(1, options.get_value("audio", "sfx", 0.0))
 	AudioServer.set_bus_volume_db(2, options.get_value("audio", "background", 0.0))
