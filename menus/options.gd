def88ec6e68533990dfc97bf40a7c6d0e2487262
Amr <amr@programmer.net>
2026-07-19 10:57:06 +0300

    rm duplicate code, they already run globally in Config

--- menus/options.gd
+++ menus/options.gd
@@ -32,25 +32,20 @@ func _on_resolution_value_changed(value: float) -> void:
 
 
 func _on_vsync_item_selected(index: int) -> void:
-	DisplayServer.window_set_vsync_mode(clamp(index, 0, 3))
 	Config.set_value("graphics", "vsync", index)
 
 
 func _on_master_audio_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(0, value)
 	Config.set_value("audio", "master", value)
 
 
 func _on_sfx_audio_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(1, value)
 	Config.set_value("audio", "sfx", value)
 
 
 func _on_ambient_audio_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(3, value)
 	Config.set_value("audio", "ambient", value)
 
 
 func _on_music_audio_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(4, value)
 	Config.set_value("audio", "music", value)
