d61ed81d41f84a29774fa6316b8e69b90c41c05a
Amr <amr@programmer.net>
2026-07-19 13:44:12 +0300

    implement power managment system

---
+++ scenes/power_cell.gd
@@ -0,0 +1,34 @@
+extends RigidBody3D
+class_name PowerCell
+
+
+@onready var player: Player = %Player
+
+var powering : Node
+
+
+func get_input_info() -> String:
+	return "[LMB] Install Power Cell\n[RMB] Drop Power Cell"
+
+
+func get_interact_info() -> String:
+	return "[E] Pick-Up Power Cell"
+
+
+func interact() -> void:
+	player.drop_item()
+	freeze = true
+	if powering: powering.remove_power_cell()
+	player.hand.remote_path = player.hand.get_path_to(self)
+
+
+func use() -> void:
+	if player.interact_ray.is_colliding():
+		var collider: CollisionObject3D = player.interact_ray.get_collider()
+		if collider.has_method("install_power_cell"):
+			if collider.install_power_cell(self):
+				player.hand.remote_path = ""
+
+
+func on_drop() -> void:
+	freeze = false
