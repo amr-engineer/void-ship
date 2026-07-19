fcc2c26269c3fc1023af5ac249fb7cc7f92c77fb
Amr <amr@programmer.net>
2026-07-19 13:15:53 +0300

    create label for version in main menu

--- menus/main.gd
+++ menus/main.gd
@@ -13,3 +13,7 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		get_viewport().set_input_as_handled()
 		quit()
+
+
+func _ready() -> void:
+	$Version.text = "v" + str(ProjectSettings.get_setting("application/config/version"))
